0943874cc0d7c90da1752df04f77a960453ddf2e
Gavin Staniforth <gsdevme@users.noreply.github.com>
2017-04-21 23:19:47 +0100

    chore: tidy up some guzzle client things

--- src/Fabric/Bridge/Guzzle/GuzzleClient.php
+++ src/Fabric/Bridge/Guzzle/GuzzleClient.php
@@ -1,17 +1,19 @@
-<?php declare(strict_types=1);
+<?php declare(strict_types = 1);
 
 namespace Gsdev\Fabric\Bridge\Guzzle;
 
 use Gsdev\Fabric\Bridge\Guzzle\Adapter\RequestToPsrAdapter;
 use Gsdev\Fabric\Model\ClientInterface;
-/*use Gsdev\Fabric\Model\Exception\ClientException;
-use Gsdev\Fabric\Model\Exception\FabricException;
-use Gsdev\Fabric\Model\Exception\InvalidResponseDataException;
-use Gsdev\Fabric\Model\Exception\UnauthorizedException;*/   
+use Gsdev\Fabric\Model\Exception\ClientException;
+use Gsdev\Fabric\Model\Exception\UnauthorizedException;
 use Gsdev\Fabric\Model\Request\RequestInterface;
 use Gsdev\Fabric\Model\Request\RequestOptionsInterface;
 use Gsdev\Fabric\Model\Response\ResponseInterface;
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\BadResponseException;
+use GuzzleHttp\Exception\ClientException as GuzzleClientException;
+use Psr\Http\Message\RequestInterface as PsrRequestInterface;
+use Psr\Http\Message\ResponseInterface as PsrResponseInterface;
 
 class GuzzleClient implements ClientInterface
 {
@@ -41,6 +43,30 @@ class GuzzleClient implements ClientInterface
             $options = $request->getOptions();
         }
 
-        $response = $this->guzzle->send($psrRequest, $options);
+        $response = $this->doRequest($psrRequest, $options);
+
+        throw new \Exception('todo');
+    }
+
+    private function doRequest(PsrRequestInterface $request, array $options): PsrResponseInterface
+    {
+        try {
+            return $this->guzzle->send($request, $options);
+        } catch (BadResponseException $e) {
+            $response = $e->getResponse();
+            $reasonPhrase = '';
+            $status = 0;
+
+            if ($response instanceof PsrResponseInterface) {
+                $status = $response->getStatusCode();
+                $reasonPhrase = $response->getReasonPhrase();
+            }
+
+            if ($e instanceof GuzzleClientException && $status === 401) {
+                throw new UnauthorizedException($reasonPhrase, 401, $e);
+            }
+
+            throw new ClientException($e->getMessage(), 0, $e);
+        }
     }
 }
